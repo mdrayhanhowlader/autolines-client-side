97c975fa4fd05e1fe4ba11a92ed99832a6ede76c
mdrayhanhowlader <rhowlader5314@gmail.com>
2022-11-30 22:31:24 +0600

    fourth answer updated

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -35,19 +35,29 @@ const Blogs = () => {
           3: What is a unit test? Why should we write unit tests?
         </h4>
         <p className="text-md font-semibold text-slate-900 mb-4">
-          JavaScript is a simple programming language that can be used with any
-          browser that has the JavaScript Engine installed. Node. js, on the
-          other hand, is an interpreter or execution environment for the
-          JavaScript programming language.
+          The main objective of unit testing is to isolate written code to test
+          and determine if it works as intended. Unit testing is an important
+          step in the development process, because if done correctly, it can
+          help detect early flaws in code which may be more difficult to find in
+          later testing stages.
         </p>
         <h4 className="text-xl font-semibold text-cyan-500">
-          4: How does NodeJS handle multiple requests at the same time??
+          4: React vs. Angular vs. Vue?
         </h4>
         <p className="text-md font-semibold text-slate-900 mb-4">
-          NodeJS receives multiple client requests and places them into
-          EventQueue. NodeJS is built with the concept of event-driven
-          architecture. NodeJS has its own EventLoop which is an infinite loop
-          that receives requests and processes them.
+          The first questions that front-end developers have to answer when
+          starting a new project are: Which framework should I choose? Which
+          framework fits better with my needs? In this article, we will give you
+          a quick overview of the most common front-end frameworks currently
+          being used, with the aim of helping you to choose the one that best
+          fulfills your needs. These frameworks are Angular, React and Vue.js.
+          These three frameworks have several things in common, such as each
+          follows a component-based architecture and allows creating UI features
+          quickly. React and Vue.js are mainly declarative, and while Angular
+          could also be declarative, it’s really more imperative. Nevertheless,
+          they present some more differences according to their structure,
+          architecture and way of working, so let’s dive into all these
+          characteristics.
         </p>
       </div>
     </div>
