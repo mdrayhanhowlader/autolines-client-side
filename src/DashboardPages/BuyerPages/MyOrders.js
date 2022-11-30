182bae636e1c156a7ecf2d3e6730d9a4ea87b8c3
mdrayhanhowlader <rhowlader5314@gmail.com>
2022-11-30 23:19:04 +0600

    orders shows on buyer dashboard

--- src/DashboardPages/BuyerPages/MyOrders.js
+++ src/DashboardPages/BuyerPages/MyOrders.js
@@ -19,6 +19,34 @@ const MyOrders = () => {
       <h2 className="text-3xl text-cyan-300 font-semibold">
         Orders: {payments.length}
       </h2>
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Booking Id</th>
+                <th>Price</th>
+                <th>Transaction Id</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {payments?.map((payment, i) => (
+                <tr key={payment._id}>
+                  <th>{i + 1}</th>
+                  <td>{payment.bookingId}</td>
+                  <td>{payment.price}</td>
+                  <td>{payment.transactionId}</td>
+                  <td>
+                    <span className="badge badge-accent">Paid</span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
