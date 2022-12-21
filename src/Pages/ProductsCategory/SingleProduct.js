6d511066e576e04ca8840b76c7bf1f73379ca27a
mdrayhanhowlader <rhowlader5314@gmail.com>
2022-12-22 04:13:31 +0600

    product card updated

--- src/Pages/ProductsCategory/SingleProduct.js
+++ src/Pages/ProductsCategory/SingleProduct.js
@@ -1,6 +1,10 @@
 import React, { useContext } from "react";
 import { GoVerified } from "react-icons/go";
 import { AuthContext } from "../../contexts/AuthProvider";
+import { BiTime } from "react-icons/bi";
+import { AiFillCar, AiOutlineUser } from "react-icons/ai";
+import { GiPriceTag } from "react-icons/gi";
+
 const SingleProduct = ({ product, handleClick }) => {
   const { user } = useContext(AuthContext);
   const {
@@ -43,35 +47,45 @@ const SingleProduct = ({ product, handleClick }) => {
           {name}
           <div className="badge badge-secondary">Used</div>
         </h2>
-        <span>Posted: {posted_time}</span>
+        <span className="flex items-center">
+          <BiTime className="fill-cyan-500" />
+          <span className="ml-2">{posted_time}</span>
+        </span>
         {/* <p>{details}</p> */}
-        <p>
-          <strong>Used: {old} Year</strong>
+        <p className="flex items-center">
+          <AiFillCar className="fill-cyan-500" />
+          <span className="ml-2">OLD: {old} Year</span>
         </p>
         <p className="flex justify-between">
-          <span>
+          {/* <span>
             <strong>Price: ${price}</strong>
-          </span>
-          <span>
-            <strong>Sale Price: ${resale_price}</strong>
+          </span> */}
+          <span className="flex items-center">
+            <GiPriceTag className="fill-cyan-500" />
+            <strong className="ml-2">${resale_price}</strong>
           </span>
         </p>
-        <p className="flex justify-between text-secondary">
+        <p className="">
           <>
             {status === "verified" ? (
               <span className="flex items-center">
                 <span>
-                  <GoVerified />
+                  <AiOutlineUser className="fill-cyan-500" />
                 </span>{" "}
-                <span> Seller: {seller}</span>
+                <span className="ml-2 flex items-center">
+                  {/* <GoVerified className="fill-green-500" /> */}
+                  <span className="ml-2 mr-2 capitalize">{seller}</span>
+                  <GoVerified className="fill-green-500" />
+                </span>
               </span>
             ) : (
-              <strong>Seller: {seller}</strong>
+              <span className="flex items-center">
+                <AiOutlineUser className="fill-cyan-500" />{" "}
+                <span className="ml-2">{seller}</span>
+              </span>
             )}
           </>
-          <span>
-            <strong>Location: {location}</strong>
-          </span>
+          <span>{/* <strong>Location: {location}</strong> */}</span>
         </p>
         <div className="card-actions justify-end">
           <div className="badge badge-outline">
